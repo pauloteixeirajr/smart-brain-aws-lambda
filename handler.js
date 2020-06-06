de153887140f77d7945f25b813df30e58ee28307
Paulo Teixeira <pauloteixeirajr@outlook.com>
2020-06-06 18:41:16 +0100

    initial commit

---
+++ handler.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const emojis = ['😃', '😄', '😆', '😇', '😍', '🥳', '🤩', '🔶', '🔷', '🚀'];
+
+module.exports.rank = async (event) => {
+  const { rank } = event.queryStringParameters;
+  const rankEmoji = emojis[rank >= emojis.length ? emojis.length - 1 : rank];
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+        rank: rankEmoji,
+      },
+      null,
+      2
+    ),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
